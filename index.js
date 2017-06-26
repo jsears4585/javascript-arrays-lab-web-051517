b0c08bd5144c4ea9ade8b2ea8928c4a2562b7676
Jason Sears <jsears4585@gmail.com>
2017-06-26 11:51:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+var destructivelyAppendKitten = (name) => {
+  return kittens.push(name);
+}
+
+var destructivelyPrependKitten = (name) => {
+  return kittens.unshift(name);
+}
+
+var destructivelyRemoveLastKitten = () => {
+  return kittens.pop();
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  return kittens.shift();
+}
+
+var appendKitten = name => {
+  var new_kittens = [...kittens, name];
+  return new_kittens;
+}
+
+var prependKitten = name => {
+  var new_kittens = [name, ...kittens];
+  return new_kittens;
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+var removeFirstKitten = () => {
+  return kittens.slice(1);
+}
